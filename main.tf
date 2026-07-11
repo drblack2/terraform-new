0bbc8c8aafc5fc5f52a569417776004251bb6bd4
Chirag Singhal <chiragsinghal236@gmail.com>
2026-07-11 15:29:26 +0530

    Add EC2 module configuration to main.tf

--- main.tf
+++ main.tf
@@ -10,3 +10,14 @@ module "s3" {
 
   bucket_name = var.bucket_name
 }
+
+module "ec2" {
+  source = "./modules/ec2"
+
+  ami           = var.ami
+  instance_type = var.instance_type
+  key_name      = var.key_name
+
+  subnet_id = module.vpc.public_subnet_id
+  vpc_id    = module.vpc.vpc_id
+}
